16f6d8d0896a8c364c4f39060a049cf515e5ecc4
Felipe Acuña <felipe_a@iMac-de-DCIUFRO-3.local>
2019-08-21 17:57:50 -0400

    Comienzos de conexion a mysql

---
+++ src/resolvers/message.js
@@ -0,0 +1,37 @@
+import uuidv4 from 'uuid/v4';
+
+const messageResolvers = {
+  Query: {
+    messages: async (parent, args, { models }) => {
+      return await models.Message.findAll();
+    },
+    message: async (parent, { id }, { models }) => {
+      return await models.Message.findByPk(id);
+    },
+  },
+
+  Mutation: {
+    createMessage: async (parent, { text }, { me, models }) => {
+      return await models.Message.create({
+        text,
+        userId: me.id,
+      });
+    },
+    updateMessage: (parent, { id, text }, { models }) => {
+      return await models.Message.update({
+        text,
+      }, { where: { id } });
+    },
+    deleteMessage: async (parent, { id }, { models }) => {
+      return await models.Message.destroy({ where: { id } });
+    },
+  },
+
+  Message: {
+    user: async (message, args, { models }) => {
+      return await models.User.findByPk(message.userId);
+    },
+  },
+};
+
+export default messageResolvers;
---
+++ src/resolvers/user.js
@@ -0,0 +1,27 @@
+import uuidv4 from 'uuid/v4';
+
+const userResolvers = {
+  Query: {
+    users: async (parent, args, { models }) => {
+      return await models.User.findAll();
+    },
+    user: async (parent, { id }, { models }) => {
+      return await models.User.findByPk(id);
+    },
+    me: (parent, args, { me }) => {
+      return await models.User.findByPk(me.id);
+    },
+  },
+
+  User: {
+    messages: async (user, args, { models }) => {
+      return await models.Message.findAll({
+        where: {
+          userId: user.id,
+        },
+      });
+    },
+  },
+};
+
+export default userResolvers;
